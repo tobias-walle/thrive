e7e83e9fb8f47f0151666eee6dbbb9aca531b761
Tobias Walle <git.tobias@walle.email>
2021-04-18 17:55:49 +0200

    test: add tests for js

--- src/js.rs
+++ src/js.rs
@@ -33,3 +33,33 @@ pub async fn exec(js: &str) -> Result<String, ExecError> {
         Err(ExecError::Output(output))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use tokio_test::block_on;
+
+    #[test]
+    fn should_run_js_and_return_stdout() {
+        let result = block_on(exec(r#"console.log("Hello World")"#));
+
+        assert_eq!(result.unwrap(), "Hello World\n")
+    }
+
+    #[test]
+    fn should_return_error_if_js_is_invalid() {
+        let result = block_on(exec(r#"invalid()"#));
+
+        match result {
+            Err(ExecError::Output(output)) => {
+                let error_message = str::from_utf8(&output.stderr).unwrap();
+                assert!(dbg!(error_message)
+                    .contains("error: Uncaught ReferenceError: invalid is not defined"));
+            }
+            _ => panic!("Unexpected result {:?}", result),
+        }
+    }
+}
